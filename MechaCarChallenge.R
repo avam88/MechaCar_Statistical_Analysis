55129116e4d5b51391827d5ae23a11bb696b5c37
Ava Mikolavich <avamikolavich@Avas-MacBook-Air.local>
2022-09-25 16:34:30 -0700

    rscript update

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -5,3 +5,11 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 suspension_coil <- read.csv('Suspension_Coil.csv')
 total_summary <- suspension_coil %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI))
 lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI), .groups = 'keep')
+
+t.test((suspension_coil$PSI),mu=1500)
+suspension_coil1 <- subset(suspension_coil, Manufacturing_Lot == "Lot1")
+t.test((suspension_coil1$PSI),mu=1500)
+suspension_coil2 <- subset(suspension_coil, Manufacturing_Lot == "Lot2")
+t.test((suspension_coil2$PSI),mu=1500)
+suspension_coil3 <- subset(suspension_coil, Manufacturing_Lot == "Lot3")
+t.test((suspension_coil3$PSI),mu=1500)
